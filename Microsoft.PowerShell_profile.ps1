258d63449329e1a5e6bda9362e4bba11e0d86889
Matheus Toniolli <matheus.toniolli@hotmail.com>
2022-03-13 11:14:20 -0400

    feat: better tab autocomplete

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -1,2 +1,6 @@
 $ENV:STARSHIP_DISTRO = "者 $env:username"
+
+# Shows navigable menu of all options when hitting Tab
+Set-PSReadlineKeyHandler -Key Tab -Function MenuComplete
+
 Invoke-Expression (&starship init powershell)
