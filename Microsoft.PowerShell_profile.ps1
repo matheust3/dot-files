767ad171589c4eba486a609bb6a6cfa9a6786d84
Matheus Toniolli <matheus.toniolli@hotmail.com>
2022-03-13 11:18:03 -0400

    feat: autosuggestions

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -3,4 +3,8 @@ $ENV:STARSHIP_DISTRO = "者 $env:username"
 # Shows navigable menu of all options when hitting Tab
 Set-PSReadlineKeyHandler -Key Tab -Function MenuComplete
 
+# Auto Suggestions
+Import-Module PSReadLine
+Set-PSReadLineOption -PredictionSource History
+
 Invoke-Expression (&starship init powershell)
